37990c8089fb9db79efc725fd3f46215e6248731
Aleksei Semochkin <aleksioprime@gmail.com>
2025-07-14 01:12:54 +0300

    set brocker redis for aledev

--- web/back/src/celery_app.py
+++ web/back/src/celery_app.py
@@ -1,10 +1,20 @@
+import os
 from celery import Celery
 from src.core.config import settings
 
+broker_type = os.environ.get("CELERY_BROKER", "rabbitmq")
+
+if broker_type == "rabbitmq":
+    broker_url = f"{settings.rabbit.url}/"
+    backend_url = f"{settings.redis.url}/0"
+else:
+    broker_url = f"{settings.redis.url}/1"
+    backend_url = f"{settings.redis.url}/0"
+
 celery_app = Celery(
     "hyperspectrus",
-    broker=f"{settings.rabbit.url}/",
-    backend=f"{settings.redis.url}/0"
+    broker=broker_url,
+    backend=backend_url
 )
 
 celery_app.autodiscover_tasks([

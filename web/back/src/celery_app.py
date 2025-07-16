b969003e7591f8406e15147dc9fba0293d8cb0c7
Aleksei Semochkin <aleksioprime@gmail.com>
2025-07-16 15:19:00 +0300

    fix celery app

--- web/back/src/celery_app.py
+++ web/back/src/celery_app.py
@@ -1,34 +1,11 @@
 import os
-import logging
-
 from celery import Celery
-
 from src.core.config import settings
-
-logger = logging.getLogger("celery_app")
-logging.basicConfig(level=logging.INFO)
-
-broker_type = os.environ.get("CELERY_BROKER", "rabbitmq")
-
-logger.info(f"CELERY_BROKER: {os.environ.get('CELERY_BROKER')}")
-logger.info(f"broker_type: {broker_type}")
-logger.info(f"settings.redis.url: {settings.redis.url}")
-logger.info(f"settings.rabbit.url: {settings.rabbit.url}")
-
-if broker_type == "rabbitmq":
-    broker_url = f"{settings.rabbit.url}/"
-    backend_url = f"{settings.redis.url}/0"
-else:
-    broker_url = f"{settings.redis.url}/1"
-    backend_url = f"{settings.redis.url}/0"
-
-logger.info(f"Final broker_url: {broker_url}")
-logger.info(f"Final backend_url: {backend_url}")
 
 celery_app = Celery(
     "hyperspectrus",
-    broker=broker_url,
-    backend=backend_url
+    broker=f"{settings.rabbit.url}/",
+    backend=f"{settings.redis.url}/0"
 )
 
 celery_app.autodiscover_tasks([

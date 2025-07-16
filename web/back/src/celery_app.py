4e2a7ffe7d6940d22bc38959307156cf09e6094a
Aleksei Semochkin <aleksioprime@gmail.com>
2025-07-16 12:38:11 +0300

    add logger to Celery

--- web/back/src/celery_app.py
+++ web/back/src/celery_app.py
@@ -1,8 +1,19 @@
 import os
+import logging
+
 from celery import Celery
+
 from src.core.config import settings
 
+logger = logging.getLogger("celery_app")
+logging.basicConfig(level=logging.INFO)
+
 broker_type = os.environ.get("CELERY_BROKER", "rabbitmq")
+
+logger.info(f"CELERY_BROKER: {os.environ.get('CELERY_BROKER')}")
+logger.info(f"broker_type: {broker_type}")
+logger.info(f"settings.redis.url: {settings.redis.url}")
+logger.info(f"settings.rabbit.url: {settings.rabbit.url}")
 
 if broker_type == "rabbitmq":
     broker_url = f"{settings.rabbit.url}/"
@@ -10,6 +21,9 @@ if broker_type == "rabbitmq":
 else:
     broker_url = f"{settings.redis.url}/1"
     backend_url = f"{settings.redis.url}/0"
+
+logger.info(f"Final broker_url: {broker_url}")
+logger.info(f"Final backend_url: {backend_url}")
 
 celery_app = Celery(
     "hyperspectrus",
